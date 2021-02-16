bdf706bb356ceb69ba55380667669c724326aa64
sean-zero1 <sean.watson@zero1.co.uk>
2021-02-16 09:20:43 +0000

    Setting default to yes

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -32,6 +32,7 @@ class InstallData implements InstallDataInterface
 				'type'         => 'int',
 				'label'        => 'Display Sub Category Listing',
 				'input'        => 'select',
+				'default'.     => '1',
 				'sort_order'   => 999,
 				'source'       => Boolean::class,
 				'global'       => ScopedAttributeInterface::SCOPE_STORE,

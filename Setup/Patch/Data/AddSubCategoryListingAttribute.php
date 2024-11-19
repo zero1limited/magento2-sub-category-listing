af0517fd980eabdefffaabeeb27a4000b5ab912c
callum-zero1 <callum.breeze@zero1.co.uk>
2024-11-19 15:54:41 +0000

    Improve attribute installation

---
+++ Setup/Patch/Data/AddSubCategoryListingAttribute.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Zero1\SubCategoryListing\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Model\Entity\Attribute\Source\Boolean;
+
+class AddSubCategoryListingAttribute implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var EavSetupFactory
+     */
+    protected $eavSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+		$eavSetup->addAttribute(
+			\Magento\Catalog\Model\Category::ENTITY,
+			'zero1_sub_category_listing',
+			[
+				'type'         => 'int',
+				'label'        => 'Display Sub Category Listing',
+				'input'        => 'select',
+				'default'      => '1',
+				'sort_order'   => 999,
+				'source'       => Boolean::class,
+				'global'       => ScopedAttributeInterface::SCOPE_STORE,
+				'group'        => 'General Information'
+			]
+		);
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
